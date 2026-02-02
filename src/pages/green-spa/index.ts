85b51b259f3e8e74d2537d9a4077d7d036e8fc53
Yuyanto <yuyanto@gmail.com>
2026-02-02 23:37:27 +0700

    deploy

--- src/pages/green-spa/index.ts
+++ src/pages/green-spa/index.ts
@@ -1,2 +1,14 @@
-// Redirect to POS as default page
-export { default } from "./pos";
+import { GetServerSideProps } from "next";
+
+// Redirect /green-spa to /green-spa/pos
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/green-spa/pos",
+      permanent: false,
+    },
+  };
+};
+
+const GreenSpaIndex = () => null;
+export default GreenSpaIndex;
